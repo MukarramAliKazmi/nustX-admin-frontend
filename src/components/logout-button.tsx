5ce714f139e967ed06802191ee5d4e7801ac14dc
Amna-Amna <mailtoamna4@gmail.com>
2023-04-13 09:46:17 +0500

    style: Add hover effect on sidebar-items, and add icon, hover effect on logout-button

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -1,7 +1,10 @@
+import { IoLogOutOutline } from "react-icons/io5";
+
 const LogoutButton = () => {
   return (
-    <button className="flex items-center w-full font-semibold text-sky-500">
+    <button className="flex items-center gap-2 w-full font-semibold text-sky-500 hover:gap-4 transition-all">
       Logout
+      <IoLogOutOutline className="w-6 h-6 fill-sky-500 " />
     </button>
   );
 };
