a65fd1beb42a3749f578beed79d09b5aee876a8a
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-04-15 23:35:47 +0500

    feat: Integrate redux toolkit to handle app state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Provider } from "react-redux";
+import store from "@/store/store";
 
 import "@/styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
